2a20bd9fa051859010f35327758d35477c1f6c0b
samau3 <samau415@gmail.com>
2021-09-28 12:36:37 -0700

    completed step1

--- step1.js
+++ step1.js
@@ -1,11 +1,17 @@
 
 const fsP = require('fs/promises')
 
+const argv = process.argv;
+
 async function cat(path) {
     try {
-
+        let contents = await fsP.readFile(path, "utf8");
+        console.log(contents);
     } catch (err) {
-        
+        console.log(err);
+        process.exit(1);
     }
 
 }
+
+cat(argv[2])
